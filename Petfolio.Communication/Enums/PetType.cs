51510a82dd0b794cc7f884dddc0193575849152a
Fernando Futila <fernando.futila@gmail.com>
2024-08-13 16:13:17 -0300

    Criação das classes de comunicação

---
+++ Petfolio.Communication/Enums/PetType.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Petfolio.Communication.Enums;
+
+public enum PetType
+{
+    Cat = 0,
+    Dog = 1
+}
---
+++ Petfolio.Communication/Responses/ResponseRegisteredPetJson.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Petfolio.Communication.Responses;
+
+public class ResponseRegisteredPetJson
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
